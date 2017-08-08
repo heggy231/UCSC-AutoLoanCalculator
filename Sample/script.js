98d07d8462b75f461d2e4eb0614929c7c94dd28f
Heggyhere <heggyy@gmail.com>
2017-08-08 16:18:18 -0700

    home

---
+++ Sample/script.js
@@ -0,0 +1,17 @@
+
+// This script is explained line by line in depth in the following video:
+// http://www.developphp.com/view.php?tid=1389
+function computeLoan(){
+	//get the values from the user
+	var amount = document.getElementById('amount').value;
+	var interest_rate = document.getElementById('interest_rate').value;
+	var months = document.getElementById('months').value;
+	//calculate the interest for the loan
+	var interest = (amount * (interest_rate * .01)) / months;
+	//toFixed(2) --> demical num with 2 digits
+	var payment = ((amount / months) + interest).toFixed(2);
+	//add a , every three digits
+	payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	//render to the html
+	document.getElementById('payment').innerHTML = "Monthly Payment = $"+payment;
+}
