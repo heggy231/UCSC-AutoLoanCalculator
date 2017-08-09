981d3ace2a1d7436b9b031f704dc4af1fba1981f
Heggyhere <heggyy@gmail.com>
2017-08-08 17:00:47 -0700

    update with notes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+//
 function computeLoan() {
+  // gather all html id  
   var amount = document.getElementById('amount').value;
-  var interest_rate = document.getElementById('interest_rate');
+  var interest_rate = document.getElementById('interest_rate').value;
   var months = document.getElementById('months').value;
   var interest = (amount * (interest_rate * .01)) / months;
   var payment = ((amount / months) + interest).toFixed(2);
