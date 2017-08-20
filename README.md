43b0f76b1f4af5bc7f604fedc304e34ac2d39327
Heggyhere <heggyy@gmail.com>
2017-08-19 20:48:33 -0700

    regex cleanup data b4 calc.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UCSC-AutoLoanCalculator
 JavaScript Comp AJAX course capstone clone bankrate.com auto loan calculator
+clone of 
+http://www.bankrate.com/calculators/auto/auto-loan-calculator.aspx
--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function computeLoan() {
   // gather all html id amount, interest_rate, months
   // each gets its.value of each fields
-  var amount = document.getElementById('amount').value;
+  var amount = document.getElementById('amount').value.toString().replace(/[^\d]/g, "");
   var interest_rate = document.getElementById('interest_rate').value;
   var months = document.getElementById('months').value;
   // below interest, payment are both calculated value
@@ -14,9 +14,33 @@ function computeLoan() {
   payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   // inputs payment in id='payment' in html
   document.getElementById('payment').innerHTML = "Monthly Payment = $" + payment;
+  
+  // filter out % from apr %
+  var apr1 = document.getElementById('apr1').textContent.toString().replace(/[^\d]/g, "");
+  console.log(apr1);
+  interest = (amount * (apr1 * .01)) / months;
+  payment = ((amount / months) + interest).toFixed(2);
+  payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  var payment1 = document.getElementById('payment1');
+  payment1.textContent = "Monthly Payment = $" + payment;
+}
+
+function onlyNumbers() {
+  var amount = document.getElementById('amount');
+  amount.value = amount.value.toString().replace(/[^\d]/g, "");
+  amount.value = amount.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 
 $.getJSON("lender.json","", function(json) {
-	console.log(json);
+  var lenderData = json; // retrieves json
+    
+  var lender1 = document.getElementById('lender1');
+  lender1.textContent = lenderData[0].lenderName;
+  
+  var apr1 = document.getElementById('apr1');
+  apr1.textContent = lenderData[0].APR + "%";
+  
+  console.log(json);
 });
+
