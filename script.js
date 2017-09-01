16d3e9f9780204c75978c788cf9f6fad3be70fd4
Heggyhere <heggyy@gmail.com>
2017-09-01 15:56:56 -0700

    Update with capital one lender info

--- script.js
+++ script.js
@@ -26,15 +26,16 @@ function computeLoan() {
   payment1.textContent = "Monthly Payment = $" + payment;
 }
 
+// allow only numbers to be input on LoanAmount
 function onlyNumbers() {
   var amount = document.getElementById('amount');
   amount.value = amount.value.toString().replace(/[^\d]/g, "");
   amount.value = amount.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-
+// retrieves json for lender1
 $.getJSON("lender.json","", function(json) {
-  var lenderData = json; // retrieves json
+  var lenderData = json;
     
   var lender1 = document.getElementById('lender1');
   lender1.textContent = lenderData[0].lenderName;
@@ -45,9 +46,26 @@ $.getJSON("lender.json","", function(json) {
   console.log(json);
 });
 
+// retrieves json for lender2
+$.getJSON("lender.json","", function(json) {
+  var lenderData = json; // retrieves json
+  
+  var lender2 = document.getElementById('lender2');
+  lender2.textContent = lenderData[1].lenderName;
+  
+  var apr2 = document.getElementById('apr2');
+  apr2.textContent = lenderData[1].APR + "%";
+  
+  console.log(json);
+});
+
+
 function clearForm () {
   // for inputs use .value, for regular textbox use .textContent
   document.getElementById('amount').value = '';
   document.getElementById('interest_rate').value = '';
-  
+  document.getElementById('lender1').value = '';
+  document.getElementById('lender2').value = '';
+  document.getElementById('apr1').value = '';
+  document.getElementById('apr2').value = '';
 }
